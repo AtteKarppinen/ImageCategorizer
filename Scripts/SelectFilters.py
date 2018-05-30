7b395b75d58c2248822d72178ca65a3aa35069d7
helioamk <t6ishe01@students.oamk.fi>
2018-05-30 13:33:04 +0300

    Select the filters
    THe for loop to handle files in a given directory (UNFINISHED)

---
+++ Scripts/SelectFilters.py
@@ -0,0 +1,29 @@
+import shutil
+
+def SelectFilters():
+    #store selected filters as strings for later use (selectedTags)
+    
+    for file in FOLDERPATH
+    #loops through the file(s) in a given folderpath, 
+    
+        if IMAGEPATH.lower().endswith(('.png', '.jpg', '.jpeg'))
+        #sends FILEPATH to  classify function and receives tagString containing 5 highest tags and their variables
+            imageClassify(FILEPATH)
+            return tagString
+        
+                if selectedTags is in tagString
+                #change the filepath/folder to corresponding tagfolder, also checks if one exists or not and creates one if
+                #neccesary.
+                   
+                    newpath = r'C:\Program Files\arbitrary'#!!!!swap the directory for this code!!! 
+                        if not os.path.exists(newpath):#if the dir doesnt exist
+                        os.makedirs(newpath)#this creates it
+                        
+                         shutil.move(src, dst, copy_function=copy2)
+                    #Recursively move a file or directory (src) to another location (dst) and return the destination.
+                    
+                
+                else
+                    pass #if no tags found in tagString, do nothing.
+        else
+            pass #do nothing if the file isnt an image file
