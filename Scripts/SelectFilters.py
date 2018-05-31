1edd331d2e9858e507f994e860d602497c858c7e
AtteKarppinen <33217173+AtteKarppinen@users.noreply.github.com>
2018-05-31 11:40:52 +0300

    First working version of this script
    Includes test function, commented out

--- Scripts/SelectFilters.py
+++ Scripts/SelectFilters.py
@@ -1,8 +1,9 @@
 import shutil
 import sys
+import os
 sys.path.append(os.path.abspath('Scripts'))
 import classify_image
-import os
+
 
 
 def SelectFilters(sourceFolder, targetFolder, filters):
@@ -11,26 +12,26 @@ def SelectFilters(sourceFolder, targetFolder, filters):
     _sourceFolder = sourceFolder
     _targetFolder = targetFolder
     
-    for _file in _sourceFolder:
+    for _file in os.listdir(_sourceFolder):
     #loops through the file(s) in a given folderpath, 
-    
         if _file.lower().endswith(('.png', '.jpg', '.jpeg')):
             #sends FILEPATH to  classify function and receives tagString containing 5 highest tags and their variables
-            _filePath = _sourceFolder + '\\' + _file
-            _tagString = str(classify_image.ClassifyImage(_filePath))
-        
-            if _filter in _tagString:
+            _imagePath = _sourceFolder + '\\' + _file
+            _tagString = str(classify_image.ClassifyImage(_imagePath))
+            if _filters in _tagString:
+                print(_filters, _tagString)
             #change the filepath/folder to corresponding tagfolder, also checks if one exists or not and creates one if
             #neccesary.
                    
-                #newpath = r'C:\Program Files\arbitrary'#!!!!swap the directory for this code!!! 
                 if not os.path.exists(_targetFolder): #if the dir doesn't exist
                     os.makedirs(_targetFolder)#this creates it
                         
-                shutil.move(src, dst, copy_function=copy2)
+                shutil.move(_imagePath, _targetFolder + '\\' + _file)
                 #Recursively move a file or directory (src) to another location (dst) and return the destination.
                     
             else:
                 pass #if no tags found in tagString, do nothing.
         else:
             pass #do nothing if the file isnt an image file
+# Test functionality       
+#SelectFilters('C:\\Users\\admin\\Desktop\\test\\Source', 'C:\\Users\\admin\\Desktop\\test\\Dest', 'vulture')
