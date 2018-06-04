a0aa5c2c184ee4250694b3a0ec5c1bde5e735949
Antonjun <antonjunttari@gmail.com>
2018-06-04 09:51:19 +0300

    button now returns the chosen path

--- ImageSortGui.py
+++ ImageSortGui.py
@@ -2,32 +2,42 @@
 import threading
 import sys
 import os
+
 # Find path for Scripts folder
 sys.path.append(os.path.abspath('Scripts'))
+
 import classify_image
-import tkinter as tk
 from subprocess import call
 from appJar import gui
-from tkinter.filedialog import askdirectory
+
 
 
 filters={"Dog":False, "Cat":False, "Bacon":False,
             "Lakeside":False, "Fire":False}
 
-#This define should print the image directory --so that it could be called in the loop. 
+
+
+#This define should print the image directory --so that it could be called in the loop.
+
+
 def pressGo(btn):
-    print("ass")
+    print(app.getEntry("d1"))
 
 
 app = gui()
 
-app.setSize(400, 600)
+
 app.addLabel("l1", "Choose a folder")
 app.addDirectoryEntry("d1")
 app.addLabel("l2", "Choose folder for filtered images")
 app.addDirectoryEntry("d2")
 app.addProperties("Choose Filters", filters )
 app.addButton("Go", pressGo)
+app.addEntry("e1")
+
+
+app.setSize(400, 600)
+
 
 app.go()
 
