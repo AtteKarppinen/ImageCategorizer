3f6c054f9518065c575e749d9fd1183376f02441
AtteKarppinen <33217173+AtteKarppinen@users.noreply.github.com>
2018-06-06 11:46:07 +0300

    Wait for boolean values
    Also wait for filters

--- Scripts/SelectFilters.py
+++ Scripts/SelectFilters.py
@@ -11,27 +11,39 @@ def SelectFilters(sourceFolder, targetFolder, filters):
     _filters = filters
     _sourceFolder = sourceFolder
     _targetFolder = targetFolder
+    print('Start Program')
     
     for _file in os.listdir(_sourceFolder):
-    #loops through the file(s) in a given folderpath, 
-        if _file.lower().endswith(('.png', '.jpg', '.jpeg')):
+    #loops through the file(s) in a given folderpath,
+        print('Reading File')
+        if _file.endswith(('.png', '.jpg', '.jpeg')):
+            print('Image File Found')
             #sends FILEPATH to  classify function and receives tagString containing 5 highest tags and their variables
             _imagePath = _sourceFolder + '\\' + _file
-            _tagString = str(classify_image.ClassifyImage(_imagePath))
-            if _filters in _tagString:
-                print(_filters, _tagString)
+            print('Classifying Image...')
+            _match = classify_image.ClassifyImage(_imagePath, _filters)
+            print('...Done! Result: ' + str(_match))
+            if _match is True:
+                print('Filter Match with: ' + _imagePath)
             #change the filepath/folder to corresponding tagfolder, also checks if one exists or not and creates one if
             #neccesary.
-                   
+                print('Check for Folders')
                 if not os.path.exists(_targetFolder): #if the dir doesn't exist
                     os.makedirs(_targetFolder)#this creates it
-                        
-                shutil.move(_imagePath, _targetFolder + '\\' + _file)
+                    
+                _filters = _filters.capitalize()    
+                if not os.path.exists(_targetFolder + '\\' + _filters + ' Pictures'): #Same as above for filtered folder
+                    os.makedirs(_targetFolder + '\\' + _filters + ' Pictures')
+                print('Moving File...')
+                    
+                shutil.move(_imagePath, _targetFolder + '\\' + _filters + ' Pictures\\' + _file)
                 #Recursively move a file or directory (src) to another location (dst) and return the destination.
+                _filters = _filters.lower()
+                print('...Done!')
                     
             else:
                 pass #if no tags found in tagString, do nothing.
         else:
             pass #do nothing if the file isnt an image file
 # Test functionality       
-#SelectFilters('C:\\Users\\admin\\Desktop\\test\\Source', 'C:\\Users\\admin\\Desktop\\test\\Dest', 'vulture')
+#SelectFilters('C:\\Users\\admin\\Desktop\\test\\Source', 'C:\\Users\\admin\\Desktop\\test\\Dest', 'animal')
